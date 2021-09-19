ed4fed22b15e9433b6aed240b2e4995ffd3dbd26
Veronica <veronica.ardilao@outlook.es>
2021-09-19 15:42:36 -0500

    i added the google maps to the form component

--- src/app/core/components/form/form.component.ts
+++ src/app/core/components/form/form.component.ts
@@ -1,20 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss']
+  styleUrls: ['./form.component.scss'],
 })
 export class FormComponent implements OnInit {
+
   
-   reactiveForm = new FormGroup ({
+  constructor(private modalService: NgbModal) { }
+
+  reactiveForm = new FormGroup ({
     name: new FormControl('', [Validators.required]),
     email: new FormControl('', [Validators.required, Validators.email]),
     body: new FormControl('', [Validators.required,Validators.minLength(30)])
   });
-
-  constructor() { }
 
   ngOnInit(): void {
   }
@@ -22,9 +24,8 @@ export class FormComponent implements OnInit {
   get f(){
     return this.reactiveForm.controls;
   }
-  
-  submit(){
-    console.log(this.reactiveForm.value);
-  }
 
+  openWindowCustomClass(content:any) {
+    this.modalService.open(content, { windowClass: 'dark-modal' });
+  }
 }
