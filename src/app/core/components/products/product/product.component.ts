94e88d08b2450ad5a41b2f97875433c3ebc16df8
Juan <juand8012@gmail.com>
2021-09-25 13:33:20 -0500

    Api HttpModule fixed

--- src/app/core/components/products/product/product.component.ts
+++ src/app/core/components/products/product/product.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { tap } from 'rxjs/operators';
+import { productI } from 'src/app/shared/interfaces';
+import { DataService } from 'src/app/shared/services/data.service';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.scss']
+  styleUrls: ['./product.component.scss'],
 })
 export class ProductComponent implements OnInit {
-
-  constructor() { }
+  public products: productI[] = [];
+  constructor(private dataSvc: DataService) {}
 
   ngOnInit(): void {
+    this.getProducts();
   }
 
+  private getProducts(): void {
+    this.dataSvc
+      .getProduct()
+      .pipe(tap((products: productI[]) => (this.products = products)))
+      .subscribe();
+  }
 }
--- src/app/shared/services/data.service.ts
+++ src/app/shared/services/data.service.ts
@@ -2,15 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { productI } from '../interfaces';
+import { environment } from '../../../environments/environment.prod';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataService {
-  private apiURL: string = 'https://finalprojectbsl.herokuapp.com';
   constructor(private http: HttpClient) {}
 
   getProduct(): Observable<productI[]> {
-    return this.http.get<productI[]>(`${this.apiURL}/product`);
+    return this.http.get<productI[]>(`${environment.apiURL}/product`);
   }
 }
