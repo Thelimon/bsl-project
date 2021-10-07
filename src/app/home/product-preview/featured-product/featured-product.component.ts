10e5ad9ba05e4d1c580ac6db884aadd9288ca968
Juan David <juand8012@gmail.com>
2021-10-07 00:00:41 -0500

    Add buttons at home section works

--- src/app/home/product-preview/featured-product/featured-product.component.ts
+++ src/app/home/product-preview/featured-product/featured-product.component.ts
@@ -1,27 +1,32 @@
-import {Component} from '@angular/core';
-import {DataService} from 'src/app/core/services/data.service';
-import {map} from 'rxjs/operators';
-
+import { Component } from '@angular/core';
+import { DataService } from 'src/app/core/services/data.service';
+import { map } from 'rxjs/operators';
+import { productI } from 'src/app/shared/interfaces';
+import { CartService } from 'src/app/core/services/cart.service';
 
 @Component({
   selector: 'app-featured-product',
   templateUrl: './featured-product.component.html',
-  styleUrls: ['./featured-product.component.scss']
+  styleUrls: ['./featured-product.component.scss'],
 })
 export class FeaturedProductComponent {
+  constructor(private dataSvc: DataService, private cartService: CartService) {}
+  
+  public featuredProducts$ = this.dataSvc.getProduct().pipe(
+    map((products) =>
+      products.filter(({ category }) => category?.includes('bestseller'))
+    ),
+    map((products) => products.slice(0, 6))
+  );
 
-  public featuredProducts$ = this.dataSvc.getProduct()
-    .pipe(
-      map(products => products.filter(({category}) => category?.includes('bestseller'))),
-      map((products) => products.slice(0, 6)),
-    );
+  public othersProducts$ = this.dataSvc.getProduct().pipe(
+    map((products) =>
+      products.filter(({ category }) => !category?.includes('bestseller'))
+    ),
+    map((products) => products.slice(0, 6))
+  );
 
-  public othersProducts$ = this.dataSvc.getProduct()
-    .pipe(
-      map(products => products.filter(({category}) => !category?.includes('bestseller'))),
-      map((products) => products.slice(0, 6)),
-    );
-
-  constructor(private dataSvc: DataService) {
+  addtocart(item: productI) {
+    this.cartService.addToCart(item);
   }
 }
